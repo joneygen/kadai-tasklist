194af9aaa5a7f5f3fd05d84932748fa8b7f7d0c9
naoya.harata <joneygennumber@gmail.com>
2020-12-06 08:17:39 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,14 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource('tasks', 'TasksController');
 // 複写機能（ボタン）
 Route::get('tasks/{id}/copy', 'TasksController@copy');
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
